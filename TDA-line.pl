6fae6429c3231e76fa2810d59173da690f3b749c
Daniel Villarroel <daniel.villarroel.m@usach.cl>
2024-05-20 21:53:56 -0400

    Avance Funciones
    Avance y correcciones en TDA's

--- TDA-line.pl
+++ TDA-line.pl
@@ -1,8 +1,24 @@
 line(Id,Name,RailType,Sections,[Id,Name,RailType,Sections]).
 
-lineLenght(Line,Length,Distance,Cost):-
-    Line(_,_,Sections,L),
-    Sections(_,_,Distancia,_,S).
+sumadis([], 0).
+sumadis([Section|Resto],TotalDistance) :-
+    getdistance(Section, Distance),
+    sumadis(Resto, RestDistance),
+    TotalDistance is Distance + RestDistance.
 
+sumacost([],0).
+sumacost([Section|Resto],TotalCost):-
+    getcost(Section,Cost),
+    sumacost(Resto, RestCost),
+    TotalCost is Cost + RestCost.
 
+largo([], 0).
+largo([_|Resto], Largo) :-
+    largo(Resto, Acc),
+    Largo is Acc + 1.
 
+lineLength(Line,Length,Distance,Cost) :-
+    line(_, _, _, Sections, Line),
+    largo(Sections,Length),
+    sumacost(Sections,Cost),
+    sumadis(Sections,Distance).
--- TDA-section.pl
+++ TDA-section.pl
@@ -1,5 +1,10 @@
 section(Point1, Point2, Distance, Cost, [Point1, Point2, Distance, Cost]).
 
+getdistance(Section, Distance) :-
+    section(_, _, Distance, _, Section).
+
+getcost(Section, Cost):-
+    section(_,_,_,Cost,Section).
 
 
 
