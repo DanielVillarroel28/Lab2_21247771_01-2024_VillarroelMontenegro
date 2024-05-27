1c22306a62b726f9e9edf4f4d27b7ce157b9df17
Daniel Villarroel <daniel.villarroel.m@usach.cl>
2024-05-26 23:51:09 -0400

    Avance TDA
    -Avance de requisitos
    -Arreglo de errores y correciones

--- TDA-pcar.pl
+++ TDA-pcar.pl
@@ -1,2 +1,15 @@
 pcar(Id,Capacity,Model,Type,[Id,Capacity,Model,Type]).
 
+getId(Pcar,Id):-
+    pcar(Id,_,_,_,Pcar).
+
+getCapacity(Pcar,Capacity):-
+    pcar(_,Capacity,_,_,Pcar).
+
+getModel(Pcar,Model):-
+    pcar(_,_,Model,_,Pcar).
+
+getType(Pcar,Type):-
+    pcar(_,_,_,Type,Pcar).
+
+
--- TDA-train.pl
+++ TDA-train.pl
@@ -1,1 +1,36 @@
 train(Id,Maker,RailType,Speed,Pcars,[Id,Maker,RailType,Speed,Pcars]).
+
+getIdT(Train,Id):-
+    train(Id,_,_,_,_,Train).
+
+getMaker(Train,Maker):-
+    train(_,Maker,_,_,_,Train).
+
+getRailType(Train,RailType):-
+    train(_,_,RailType,_,_,Train).
+
+getSpeed(Train,Speed):-
+    train(_,_,_,Speed,_,Train).
+
+
+getPcars(Train,Pcars):-
+    train(_,_,_,_,Pcars,Train).
+
+
+trainAddCar(Train,Pcar,Position,Train_New):-
+    getIdT(Train,Id),
+    getMaker(Train,Maker),
+    getRailType(Train,RailType),
+    getSpeed(Train,Speed),
+    getPcars(Train,Pcars),
+    train(Id,Maker,RailType,Speed,[Pcars|Pcar],Train_New).
+
+sumaCapacity([],0).
+sumaCapacity([Pcar|Resto],TotalCapacity):-
+    getCapacity(Pcar,Capacity),
+    sumaCapacity(Resto,RestCapacity),
+    TotalCapacity is Capacity + RestCapacity.
+
+trainCapacity(Train,C):-
+    getPcars(Train,Pcars),
+    sumaCapacity(Pcars,C).
